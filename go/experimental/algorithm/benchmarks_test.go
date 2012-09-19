c41afe310107e4631406be947197aa48aac0b989
Mike Bland <mbland@acm.org>
2012-09-19 16:09:02 -0400

    Algorithm benchmarks for compound types.
    The benchmarks focus on ElementsEqual() since that was the algorithm most
    affected by earlier benchmarks using native strings. This benchmark
    demonstrates that the generic algorithm continues to be slow even for compound
    types, and a full order of magnitude slower for comparisons based solely on
    integer members:
    
    OrganNameElementsEqualHardcoded():    1.9x faster
    OrganWeightElementsEqualHardcoded(): 10.2x faster
    OrganElementsEqualHardcoded():        2.0x faster
    
    This suggests to me that, for STL-style algorithms at least, generics would
    produce a significant performance benefit. Then again, is it so hard to roll
    your own version of the algorithms you really need for your own custom type?
    
    $ go test -bench 'Organ' code.google.com/p/mike-bland/go/experimental/algorithm
    PASS
    
    BenchmarkOrganNameElementsEqualHardcoded     10000000  241 ns/op
    BenchmarkOrganNameElementsEqualGeneric        5000000  446 ns/op
    
    BenchmarkOrganWeightElementsEqualHardcoded  100000000  23.4 ns/op
    BenchmarkOrganWeightElementsEqualGeneric     10000000  238 ns/op
    
    BenchmarkOrganElementsEqualHardcoded         10000000  248 ns/op
    BenchmarkOrganElementsEqualGeneric            5000000  487 ns/op
    
    ok      code.google.com/p/mike-bland/go/experimental/algorithm  16.029s

--- go/experimental/algorithm/benchmarks_test.go
+++ go/experimental/algorithm/benchmarks_test.go
@@ -69,3 +69,176 @@ func BenchmarkSetIntersectionUnorderedStringsExp(b *testing.B) {
 		SetIntersectionUnorderedStrings(lhs, rhs)
 	}
 }
+
+// Same example types as taken from the standard sort package example, plus
+// different Comparators. These will be used to implement different versions
+// of ElementsEqual() for Organ types, and compare the performance of the
+// hardcoded versions with the generic ElementsEqual() that uses Comparators.
+type (
+	Grams int
+	Organ struct {
+		Name   string
+		Weight Grams
+	}
+	Organs                []*Organ
+	OrganNameComparator   struct{ Lhs, Rhs Organs }
+	OrganWeightComparator struct{ Lhs, Rhs Organs }
+	OrganComparator       struct{ Lhs, Rhs Organs }
+)
+
+func NewOrganNameComparator(lhs, rhs Organs) *OrganNameComparator {
+	return &OrganNameComparator{lhs, rhs}
+}
+
+func (c *OrganNameComparator) Lengths() (len_lhs, len_rhs int) {
+	return len(c.Lhs), len(c.Rhs)
+}
+
+func (c *OrganNameComparator) LhsLess(i, j int) bool {
+	return c.Lhs[i].Name < c.Rhs[j].Name
+}
+
+func (c *OrganNameComparator) RhsLess(i, j int) bool {
+	return c.Rhs[j].Name < c.Lhs[i].Name
+}
+
+func (c *OrganNameComparator) Equal(i, j int) bool {
+	return c.Lhs[i].Name == c.Rhs[j].Name
+}
+
+func NewOrganWeightComparator(lhs, rhs Organs) *OrganWeightComparator {
+	return &OrganWeightComparator{lhs, rhs}
+}
+
+func (c *OrganWeightComparator) Lengths() (len_lhs, len_rhs int) {
+	return len(c.Lhs), len(c.Rhs)
+}
+
+func (c *OrganWeightComparator) LhsLess(i, j int) bool {
+	return c.Lhs[i].Weight < c.Rhs[j].Weight
+}
+
+func (c *OrganWeightComparator) RhsLess(i, j int) bool {
+	return c.Rhs[j].Weight < c.Lhs[i].Weight
+}
+
+func (c *OrganWeightComparator) Equal(i, j int) bool {
+	return c.Lhs[i].Weight == c.Rhs[j].Weight
+}
+
+func NewOrganComparator(lhs, rhs Organs) *OrganComparator {
+	return &OrganComparator{lhs, rhs}
+}
+
+func (c *OrganComparator) Lengths() (len_lhs, len_rhs int) {
+	return len(c.Lhs), len(c.Rhs)
+}
+
+func (c *OrganComparator) LhsLess(i, j int) bool {
+	return c.Lhs[i].Name < c.Rhs[j].Name ||
+		(c.Lhs[i].Name == c.Rhs[j].Name &&
+			c.Lhs[i].Weight < c.Rhs[j].Weight)
+}
+
+func (c *OrganComparator) RhsLess(i, j int) bool {
+	return c.Rhs[j].Name < c.Lhs[i].Name ||
+		(c.Rhs[j].Name == c.Lhs[i].Name &&
+			c.Rhs[j].Weight < c.Lhs[i].Weight)
+
+}
+
+func (c *OrganComparator) Equal(i, j int) bool {
+	return c.Lhs[i].Name == c.Rhs[j].Name &&
+		c.Lhs[i].Weight == c.Rhs[j].Weight
+}
+
+func OrganNameElementsEqual(lhs, rhs Organs) bool {
+	if len(lhs) != len(rhs) {
+		return false
+	}
+	for i, v := range lhs {
+		if v.Name != rhs[i].Name {
+			return false
+		}
+	}
+	return true
+}
+
+func OrganWeightElementsEqual(lhs, rhs Organs) bool {
+	if len(lhs) != len(rhs) {
+		return false
+	}
+	for i, v := range lhs {
+		if v.Weight != rhs[i].Weight {
+			return false
+		}
+	}
+	return true
+}
+
+func OrganElementsEqual(lhs, rhs Organs) bool {
+	if len(lhs) != len(rhs) {
+		return false
+	}
+	for i, v := range lhs {
+		if v.Name != rhs[i].Name || v.Weight != rhs[i].Weight {
+			return false
+		}
+	}
+	return true
+}
+
+var (
+	lhs_organs = Organs{
+		{"brain", 1340},
+		{"heart", 290},
+		{"liver", 1494},
+		{"pancreas", 131},
+		{"prostate", 62},
+		{"spleen", 162},
+	}
+	rhs_organs = Organs{
+		{"brain", 1340},
+		{"heart", 290},
+		{"liver", 1494},
+		{"pancreas", 131},
+		{"prostate", 62},
+		{"spleen", 162},
+	}
+)
+
+func BenchmarkOrganNameElementsEqualHardcoded(b *testing.B) {
+	for i := 0; i != b.N; i++ {
+		OrganNameElementsEqual(lhs_organs, rhs_organs)
+	}
+}
+
+func BenchmarkOrganNameElementsEqualGeneric(b *testing.B) {
+	for i := 0; i != b.N; i++ {
+		ElementsEqual(NewOrganNameComparator(lhs_organs, rhs_organs))
+	}
+}
+
+func BenchmarkOrganWeightElementsEqualHardcoded(b *testing.B) {
+	for i := 0; i != b.N; i++ {
+		OrganWeightElementsEqual(lhs_organs, rhs_organs)
+	}
+}
+
+func BenchmarkOrganWeightElementsEqualGeneric(b *testing.B) {
+	for i := 0; i != b.N; i++ {
+		ElementsEqual(NewOrganWeightComparator(lhs_organs, rhs_organs))
+	}
+}
+
+func BenchmarkOrganElementsEqualHardcoded(b *testing.B) {
+	for i := 0; i != b.N; i++ {
+		OrganElementsEqual(lhs_organs, rhs_organs)
+	}
+}
+
+func BenchmarkOrganElementsEqualGeneric(b *testing.B) {
+	for i := 0; i != b.N; i++ {
+		ElementsEqual(NewOrganComparator(lhs_organs, rhs_organs))
+	}
+}
